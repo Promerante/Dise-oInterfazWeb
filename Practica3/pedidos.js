a4beb18c85c52fb8852c42f87d7ff0a6bdef342c
unknown <facundodanielbenitez@gmail.com>
2025-01-29 13:50:15 +0100

    Practica 3 Mejorada

--- Practica3/pedidos.js
+++ Practica3/pedidos.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
     }
     if (tabla.find("tr").length == 1) {
       //*CASO DE QUE LA TABLA PIERDA SU ULTIMO ELEMENTO PASANDOLO A LA OTRA
-      alert("Has pulsado para archivar la ultima fila que tenia la tabla");
+      alert("Has pulsado para cambiar la ultima fila que tenia la tabla");
       tabla.closest("div").css("display", "none");
       console.log(tabla.find("tr").length);
     }
@@ -40,17 +40,57 @@ $(document).ready(function () {
       $(this).closest("tr").remove();
     }
   });
-  $("#archivarTabla").click(function(){
-    var tablaPendiente=$("#pendiente").find("tr");
-    var tablaArchivados=$("#archivados") 
-    $.each(tablaPendiente,function(nodo,valor){
-        if(nodo!=0){
-            //Para que no tome el nodo donde esta puesto id,imagen,fecha....
-            tablaArchivados.append(valor);
-        }
-    })
-    $("#contArchivados").css("display","block")
-    $("#contPendiente").css("display","none")
-  })
+  $("#archivarTabla").click(function () {
+    var tablaPendiente = $("#pendiente").find("tr");
+    var tablaArchivados = $("#archivados");
+    $.each(tablaPendiente, function (nodo, valor) {
+      if (nodo != 0) {
+        //Para que no tome el nodo donde esta puesto id,imagen,fecha....
+        tablaArchivados.append(valor);
+      }
+    });
+    $("#contArchivados").css("display", "block");
+    $("#contPendiente").css("display", "none");
+  });
 
+  $("#cambiarTema").click(function () {
+    let fondo = $("body").css("background-color");
+    if (fondo == "rgb(0, 0, 0)") {
+      // *CASO fondo oscuro
+      $("body").addClass("bodyTemaClaro");
+      $("nav").toggleClass("navegacionClaro");
+      $(".contLogo img").attr("src", "imagenes/logo/logoICONRAW.png");
+      $(".contLogo img").css({
+        width: "75px",
+        height: "75px",
+      });
+      $(".menuElemento").toggleClass("menuElementoClaro");
+      $(".contCarrito img").attr("src", "imagenes/carrito.png");
+      $(".contBuscador").css("background-color","#c3c3c3")
+      $("footer").css("background-color","#c3c3c3")
+      $(".tablaPedidos th, .tablaPedidos td").css("border","2px solid #000000")
+      $(".tablaArchivados th, .tablaPedidos td").css("border","2px solid #000000")
+      $(".archivarTabla").toggleClass("archivarTablaClaro")
+  
+    } else {
+      // *CASO fondo claro
+      $("body").removeClass("bodyTemaClaro");
+      $("nav").toggleClass("navegacionClaro");
+      $(".contLogo img").attr("src", "imagenes/logo/logo1Rectangulo.png");
+      $(".contLogo img").css({
+        width: "150px",
+        height: "auto",
+      });
+      $(".menuElemento").toggleClass("menuElementoClaro");
+      $(".contCarrito img").attr("src", "imagenes/carritoBlanco.png");
+      $(".contBuscador").css("background-color","#242527")
+      $("footer").css("background-color","#242527")
+      $(".tablaPedidos th, .tablaPedidos td").css("border","2px solid whitesmoke")
+      $(".tablaArchivados th, .tablaPedidos td").css("border","2px solid whitesmoke")
+
+// MIRAR ESTO:SIGUE SIN VOLVER TOTALMENTE A SU ESTADO ORIGINAL
+      $(".archivarTablaClaro").toggleClass("archivarTabla")
+      
+    }
+  });
 });
